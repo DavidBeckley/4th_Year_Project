2eefdf4d3a03cf26a9aed9610a968f2de79389c3
DavidBeckley <david.beckley.x00080130@gmail.com>
2015-03-31 16:20:41 +0100

    Newest Version of Project
    Merge Conflict Error

---
+++ Google_Maps_Transport_Project/Google_Maps_Transport_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Google_Maps_Transport_Project.Startup))]
+namespace Google_Maps_Transport_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
